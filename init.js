7e91e9bf3c78ff3dcbaf761fce65b993fb1c06fe
Shaurya Tripathi <88775162+shauryatripaathi@users.noreply.github.com>
2022-01-06 22:15:41 +0530

    Update init.js

--- init.js
+++ init.js
@@ -11,6 +11,7 @@ let tile33 = document.getElementById("tile33");
 let playerInfoBox = document.getElementById("playerInfo");
 playerInfoBox.innerText = "LET'S START THE GAME -- Player 1's turn";
 
+
 const mark = ["O", "X"];
 let val = 0;
 function handleOnGridClick(e) {
@@ -25,6 +26,8 @@ function handleOnGridClick(e) {
     console.log(val);
     playerInfoBox.innerText = `Player ${val+1}'s turn`;
 
+    checkIfPlayerWon();
+    
     checkIfGameIsOver();
 }
 
@@ -42,9 +45,46 @@ function checkIfGameIsOver() {
         tile32.innerText != "" &&
         tile33.innerText != ""
     ) {
+        playerInfoBox.innerText = "Game Over"
         setTimeout(function () {
             alert("----Game Over----")
             location.reload();
         });
     }
+}
+
+function checkIfPlayerWon(){
+if(
+   tile11.innerText == "X" && tile12.innerText == "X" && tile13.innerText == "X" ||
+   tile21.innerText == "X" && tile22.innerText == "X" && tile23.innerText == "X" ||
+   tile31.innerText == "X" && tile32.innerText == "X" && tile33.innerText == "X" ||
+   tile11.innerText == "X" && tile21.innerText == "X" && tile31.innerText == "X" ||
+   tile12.innerText == "X" && tile22.innerText == "X" && tile32.innerText == "X" ||
+   tile13.innerText == "X" && tile23.innerText == "X" && tile33.innerText == "X" ||
+   tile31.innerText == "X" && tile22.innerText == "X" && tile13.innerText == "X" ||
+   tile11.innerText == "X" && tile22.innerText == "X" && tile33.innerText == "X"
+    ){
+    playerInfoBox.innerText = "Player 1 Won";
+    setTimeout(function(){
+        alert("Player 1 Won");
+        location.reload();
+    });
+    }   
+
+else if(
+   tile11.innerText == "O" && tile12.innerText == "O" && tile13.innerText == "O" ||
+   tile21.innerText == "O" && tile22.innerText == "O" && tile23.innerText == "O" ||
+   tile31.innerText == "O" && tile32.innerText == "O" && tile33.innerText == "O" ||
+   tile11.innerText == "O" && tile21.innerText == "O" && tile31.innerText == "O" ||
+   tile12.innerText == "O" && tile22.innerText == "O" && tile32.innerText == "O" ||
+   tile13.innerText == "O" && tile23.innerText == "O" && tile33.innerText == "O" ||
+   tile31.innerText == "O" && tile22.innerText == "O" && tile13.innerText == "O" ||
+   tile11.innerText == "O" && tile22.innerText == "O" && tile33.innerText == "O"
+    ){
+    playerInfoBox.innerText = "Player 2 Won";
+    setTimeout(function(){
+        alert("Player 2 Won");
+        location.reload();
+    });
+    }    
 }
